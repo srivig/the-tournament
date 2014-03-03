3324acc0d18be0702cff76dc56c0d63e1f261745
tomomichi <tomomichi.onishi@gmail.com>
2014-03-04 08:19:03 +0900

    change player edit pages, delete static index/show

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,5 +1,34 @@
 class PlayersController < ApplicationController
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+
   def index
     @tournament = Tournament.find(params[:tournament_id])
   end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @player.update(player_params)
+        format.html { redirect_to tournament_players_path(@player.tournament), notice: 'Player was successfully updated.' }
+        format.json { head :no_content }
+      else
+        flash.now[:alert] = "Failed on saving the player."
+        format.html { render action: 'edit' }
+        format.json { render json: @player.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_player
+      @player = Player.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def player_params
+      params.require(:player).permit(:id, :tournament_id, :seed, :name, :group)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ TheTournament::Application.routes.draw do
     resources :games
   end
   resources :games
+  resources :players
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
