95c374fb93ff5a3f08fa733c5dc6da72b46cf95b
tomomichi <tomomichi.onishi@gmail.com>
2014-03-05 16:04:29 +0900

    mock design for side bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def profile_img(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    img_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(img_url, alt: user.email, class: 'img-circle')
+  end
 end
