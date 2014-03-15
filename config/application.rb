8b078430b7f50dfa9899a5828005ad396d7175a1
tomomichi <tomomichi.onishi@gmail.com>
2014-03-15 15:33:24 +0900

    silence i18n enforce deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
@@ -24,5 +25,6 @@ module TheTournament
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.enforce_available_locales = false
   end
 end
