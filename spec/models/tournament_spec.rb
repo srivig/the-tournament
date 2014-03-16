54132b8662292e2962105d012392406a6b19444e
tomomichi <tomomichi.onishi@gmail.com>
2014-03-16 14:15:32 +0900

    finish tournament specs

--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -27,17 +27,68 @@ describe Tournament do
     end
   end
 
+  describe 'create_players' do
+    before :each do
+      @user = create(:user)
+      @tournament = create(:tournament, user:@user, size:8)
+    end
+
+    it 'should have players equal to its size' do
+      expect(@tournament.players.size).to eq(8)
+    end
+  end
+
+  describe 'create_games' do
+    context 'when it has a third-party placement' do
+      before :each do
+        @user = create(:user)
+        @tournament = create(:tournament, user:@user, size:8)
+      end
+
+      it 'should have right number of games' do
+        expect(@tournament.games.size).to eq(8)
+      end
+    end
+  end
+
   describe 'round_num' do
     before :each do
       @user = create(:user)
     end
 
-    it 'should return the correct round num' do
-      [2,3,4].each do |i|
-        tournament = create(:tournament, user:@user, size:2**i)
-        expect(tournament.round_num).to eq(i)
+    context 'when it has a third-party placement' do
+      it 'should return the correct round num' do
+        [2,3,4].each do |i|
+          tournament = create(:tournament, user:@user, size:2**i)
+          expect(tournament.round_num).to eq(i)
+        end
       end
     end
   end
 
+  describe 'third-place' do
+    before :each do
+      @user = create(:user)
+    end
+
+    context 'when it has a third-party placement' do
+      it 'should return the game for the third-place playoff' do
+        @tournament = create(:tournament, user:@user, size:8)
+        expect(@tournament.third_place).to eq(@tournament.games.find_by(bracket:1, round:3, match:2))
+      end
+    end
+  end
+
+  describe 'set_first_rounds' do
+    before :each do
+      @user = create(:user)
+      @tournament = create(:tournament, user:@user)
+    end
+
+    it 'should have first-round game with game_records' do
+      @tournament.games.where(round:1).each do |game|
+        expect(game.game_records.size).to be > 0
+      end
+    end
+  end
 end
