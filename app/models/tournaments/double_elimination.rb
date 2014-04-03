aca415ab1123797dc47c7acdad225de2b7612dcf
tomomichi <tomomichi.onishi@gmail.com>
2014-04-03 16:50:25 +0900

    Add game type on saving

--- app/models/tournaments/double_elimination.rb
+++ app/models/tournaments/double_elimination.rb
@@ -3,15 +3,15 @@ class DoubleElimination < Tournament
 
   # return a game of the third-place playoff
   def third_place
-    Game.find_by(tournament: self, bracket:3, round:1, match:2)
+    self.games.find_by(bracket:3, round:1, match:2)
   end
 
   def final
-    Game.find_by(tournament: self, bracket:3, round:1, match:1)
+    self.games.find_by(bracket:3, round:1, match:1)
   end
 
   def build_third_place_game
-    self.games.build(bracket:3, round:1, match:2)
+    self.games.build(bracket:3, round:1, match:2, type: 'Final')
   end
 
   def build_loser_and_final_games
@@ -19,11 +19,11 @@ class DoubleElimination < Tournament
     for i in 1..loser_round_num do
       match_num_base = (loser_round_num+1-i).quo(2).ceil - 1  #2ラウンドごとに試合数が変わる(e.g. 4-4-2-2-1-1)
       (2**match_num_base).times do |k|
-        self.games.build(bracket:2, round:i, match:k+1)
+        self.games.build(bracket:2, round:i, match:k+1, type: 'Loser')
       end
     end
     2.times do |i|
-      self.games.build(bracket:3, round:i+1, match:1) #決勝戦(secondary finalも作っておく)
+      self.games.build(bracket:3, round:i+1, match:1, type: 'Final') #決勝戦(secondary finalも作っておく)
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401124342) do
+ActiveRecord::Schema.define(version: 20140403074346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20140401124342) do
     t.boolean  "bye"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type",          default: "Winner"
   end
 
   create_table "players", force: true do |t|
