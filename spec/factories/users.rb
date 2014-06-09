a8b91d519fa95d374acedc689508bc9cd2d20ac0
tomomichi <tomomichi.onishi@gmail.com>
2014-06-09 17:20:39 +0900

    add spec for user.creatable

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,12 @@ require 'faker'
 
 FactoryGirl.define do
   factory :user do
-    email "hogehoge@gmail.com"
+    sequence :email do |n|
+      "hogehoge#{n}@example.com"
+    end
     password "hogehoge"
     password_confirmation "hogehoge"
     admin false
-    accept_terms true
     # provider "facebook"
     # uid { Faker::Number.number(10) }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,15 +9,31 @@ describe Tournament do
   describe 'creatable?' do
     context 'admin' do
       it 'should be true with less than 3 tournaments' do
-        create(:tournament, user:@admin)
+        create(:se_tnmt, user:@admin)
         expect(@admin.creatable?).to be_true
       end
 
       it 'should be true with more than 3 tournaments' do
         5.times do
-          create(:tournament, user:@admin)
+          create(:se_tnmt, user:@admin)
         end
         expect(@admin.creatable?).to be_true
+      end
+    end
+
+    context 'non-paid user' do
+      it 'should be true with less than 3 tournaments' do
+        2.times do
+          create(:se_tnmt, user:@user)
+        end
+        expect(@user.creatable?).to be_true
+      end
+
+      it 'should be true with more than 3 tournaments' do
+        3.times do
+          create(:se_tnmt, user:@user)
+        end
+        expect(@user.creatable?).to be_false
       end
     end
   end
