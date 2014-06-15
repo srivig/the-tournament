48ec63c535c98bf71a7f2293ab680229979044a9
tomomichi <tomomichi.onishi@gmail.com>
2014-06-15 15:23:35 +0900

    Add email subscription option

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,33 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :edit, :update]
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to user_path(@user), notice: 'Success on updating user info.' }
+        format.json { head :no_content }
+      else
+        flash.now[:alert] = 'Failed on updating user info.'
+        format.html { render edit_user_path(@user) }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
     def set_user
       @user = User.find(params[:id])
       @tournaments = @user.tournaments.all
+    end
+
+    def user_params
+      params.require(:user).permit(:email, :email_subscription)
     end
 end
