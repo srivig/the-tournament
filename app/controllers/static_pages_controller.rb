903a667aff68bb6e3acb373349c75636f4b47c0c
tomomichi <tomomichi.onishi@gmail.com>
2016-05-15 14:02:28 +0900

    fix pickup logic

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -18,7 +18,7 @@ class StaticPagesController < ApplicationController
   end
 
   def top
-    @tournament = Tournament.where(pickup: true).last
+    @tournament = Tournament.where(pickup: true).order(created_at: :desc).first
     @tournaments = Tournament.finished.limit(5)
   end
 
