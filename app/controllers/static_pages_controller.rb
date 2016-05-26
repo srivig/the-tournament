da076224a1fda43ad1fce2d7e471dda730eeb529
tomomichi <tomomichi.onishi@gmail.com>
2016-05-26 17:26:12 +0900

    show blog rss

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -26,6 +26,6 @@ class StaticPagesController < ApplicationController
   private
 
     def set_blog_rss
-      #@rss = SimpleRSS.parse open('http://blog.the-tournament.jp/feed.xml')
+      @rss= SimpleRSS.parse open('http://blog.the-tournament.jp/rss')
     end
 end
