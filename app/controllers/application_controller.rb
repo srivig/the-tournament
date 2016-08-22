b0fc04a69f2af49bd2a13a4920ff9ff2bd1bcda9
tomomichi <tomomichi.onishi@gmail.com>
2016-08-22 13:01:52 +0900

    :bug: fix devise sanitize error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale, :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  #before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options(options={})
     {locale: I18n.locale}
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << :accept_terms
+      devise_parameter_sanitizer.permit(:sign_up) << :accept_terms
     end
 end
