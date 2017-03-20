de367e9ed7fb0999e76424e65337f7bea5ffc807
tomomichi <tomomichi.onishi@gmail.com>
2017-03-20 23:35:07 +0900

    restore blog feed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -28,6 +28,6 @@ class StaticPagesController < ApplicationController
   private
 
     def set_blog_rss
-      #@rss= SimpleRSS.parse open('https://blog.the-tournament.jp/feed')
+      @rss= SimpleRSS.parse open('http://blog.the-tournament.jp/feed')
     end
 end
